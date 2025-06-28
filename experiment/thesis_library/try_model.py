dac4c4d530cf3d6a564737a8d72ff25fb9094e8e
Pystronic <pystronic@gmail.com>
2025-06-28 15:31:26 +0200

    Add file to test model setup locally

---
+++ experiment/thesis_library/try_model.py
@@ -0,0 +1,89 @@
+# Import the required modules
+from pathlib import Path
+
+import torch
+from anomalib.callbacks import ModelCheckpoint
+from anomalib.data import MVTecAD
+from anomalib.data.utils import ValSplitMode
+from anomalib.deploy import ExportType
+from anomalib.metrics import Evaluator
+from anomalib.models import EfficientAd
+from anomalib.engine import Engine
+from anomalib.models.image.efficient_ad.torch_model import EfficientAdModelSize
+from lightning.pytorch.loggers import CSVLogger
+from pandas import DataFrame
+
+from thesis_library.metrics import image, pixel
+from thesis_library.metrics.util import  calculate_AD_metrics
+
+# Fix pytorch-lightning bug
+# https://github.com/Lightning-AI/pytorch-lightning/issues/17124
+def getstate_patch(*_):
+    return {}
+from torch.utils.data.dataloader import _BaseDataLoaderIter
+_BaseDataLoaderIter.__getstate__ = getstate_patch
+
+
+
+
+OUTPUT_PATH = Path("./results")
+LOAD_MODEL_PATH = None # OUTPUT_PATH / "weights/torch/EfficientAd.pt"
+
+# Initialize the datamodule, model, and engine
+
+datamodule = MVTecAD(train_batch_size=1, eval_batch_size=1, val_split_mode=ValSplitMode.FROM_TEST, val_split_ratio=0.2)
+
+# Initialize metrics
+test_metrics = [
+    *image.get_metrics(),
+    *pixel.get_metrics(),
+]
+# Reference needed later for workaround
+
+logger = CSVLogger(OUTPUT_PATH / "logs", name="run_log")
+
+
+evaluator = Evaluator(
+    test_metrics=test_metrics,
+    val_metrics=pixel.get_val_metrics(),
+    compute_on_cpu=True,
+)
+
+checkpoint_callback = ModelCheckpoint(
+    dirpath="model/chk",
+    filename="best-{epoch:02d}-{PX_AUROC:.3f}",
+    monitor="PX_AUROC",
+    mode="max",
+    save_top_k=3
+)
+
+engine = Engine(max_epochs=1,
+                callbacks=[checkpoint_callback],
+                logger=logger,
+                default_root_dir=OUTPUT_PATH,
+)
+
+# Specify backbone and layers
+
+model = EfficientAd(evaluator=evaluator, model_size=EfficientAdModelSize.S)
+if LOAD_MODEL_PATH is not None:
+    # Load model weight we can trust, since we exported it ourselves
+    model.load_state_dict(torch.load(LOAD_MODEL_PATH, weights_only=False), strict=False)
+
+
+
+# Train the model
+if LOAD_MODEL_PATH is None:
+    engine.fit(datamodule=datamodule, model=model)
+    engine.export(model, ExportType.TORCH, model_file_name="EfficientAd", export_root=OUTPUT_PATH)
+
+# Returned as single element list
+test_result = engine.test(datamodule=datamodule, model=model)[0]
+
+print("##############")
+test_result = calculate_AD_metrics(test_result)
+print(test_result)
+
+# Export result metrics
+result_frame = DataFrame.from_records([dict(test_result)])
+result_frame.to_csv(OUTPUT_PATH / "test_results.csv")
