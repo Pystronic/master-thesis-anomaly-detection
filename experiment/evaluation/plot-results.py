e569fd767aa4245d3e2ef0f68868b7326699d540
Pystronic <pystronic@gmail.com>
2025-08-06 15:27:34 +0200

    Analyze metrics in evaluation

--- experiment/evaluation/plot-results.py
+++ experiment/evaluation/plot-results.py
@@ -33,14 +33,48 @@ for category in categories:
     plt.title(f'Boxplots für {category}')
     plt.show()
 
+# Analyse each metric per category
+categories = np.array([])
+metrics = np.array([])
+mins = np.array([])
+min_models = np.array([])
+maxes = np.array([])
+max_models = np.array([])
+variances = np.array([])
+means = np.array([])
+
 for (category, metric) in itertools.product(categories, metric_columns):
     filtered = frame.loc[frame['category'] == category]
     metric_values = np.array([list(filtered.loc[filtered['model'] == model][metric]) for model in models])
 
-    # ToDo: min, max, variance, mean
-    # TodO: plot per model
+    min = metric_values.min()
+    max = metric_values.max()
+    min_model = models[metric_values.argwhere(lambda x: x == min)]
+    max_model = models[metric_values.argwhere(lambda x: x == max)]
 
-# ToDo:
+    categories.append(category)
+    metrics.append(metric)
+    mins.append(min)
+    min_models.append(min_model)
+    maxes.append(max)
+    max_models.append(max_model)
+    variances.append(metric_values.var())
+    means.append(metric_values.mean())
+
+    plt.bar(metric_values, tick_label=models)
+    plt.title(f'Werte pro Model für {metric} in {category}')
+    plt.show()
+
+frame = pd.DataFrame({
+    'Category': categories,
+    'Metric': metrics,
+    'Min': mins,
+    'Min Model': min_models,
+    'Max': maxes,
+    'Max Models': max_models,
+    'Variance': variances,
+    'Mean': means
+})
+frame.to_csv('./metric_evaluation.csv')
+
 # Pro categorie + metrik, plot zum vergleich der Werte der Modelle
-# Max + Min Wert & Model,
-# Varianz + Mean berechnen & im Plot ausgeben
