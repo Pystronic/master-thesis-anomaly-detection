11c00c32d41e5631c9dbf82a50763520f7ec2d0b
Pystronic <pystronic@gmail.com>
2025-08-07 12:40:00 +0200

    Add bugged metric evaulation code

--- experiment/evaluation/plot-results.py
+++ experiment/evaluation/plot-results.py
@@ -34,7 +34,7 @@ for category in categories:
     plt.show()
 
 # Analyse each metric per category
-categories = np.array([])
+evaluated_categories = np.array([])
 metrics = np.array([])
 mins = np.array([])
 min_models = np.array([])
@@ -44,29 +44,30 @@ variances = np.array([])
 means = np.array([])
 
 for (category, metric) in itertools.product(categories, metric_columns):
+    print(f'Category {category}   Metric {metric}')
     filtered = frame.loc[frame['category'] == category]
     metric_values = np.array([list(filtered.loc[filtered['model'] == model][metric]) for model in models])
 
     min = metric_values.min()
     max = metric_values.max()
-    min_model = models[metric_values.argwhere(lambda x: x == min)]
-    max_model = models[metric_values.argwhere(lambda x: x == max)]
+    #min_model = models[metric_values.argwhere(lambda x: x == min)]
+    #max_model = models[metric_values.argwhere(lambda x: x == max)]
 
-    categories.append(category)
-    metrics.append(metric)
-    mins.append(min)
-    min_models.append(min_model)
-    maxes.append(max)
-    max_models.append(max_model)
-    variances.append(metric_values.var())
-    means.append(metric_values.mean())
+    evaluated_categories+= [category]
+    metrics+= [metric]
+    mins+= [min]
+    #min_models+= [min_model]
+    maxes+= [max]
+    #max_models+= [max_model]
+    variances+= [metric_values.var()]
+    means+= [metric_values.mean()]
 
     plt.bar(metric_values, tick_label=models)
     plt.title(f'Werte pro Model für {metric} in {category}')
     plt.show()
 
 frame = pd.DataFrame({
-    'Category': categories,
+    'Category': evaluated_categories,
     'Metric': metrics,
     'Min': mins,
     'Min Model': min_models,
